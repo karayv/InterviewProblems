1c574a1c601538fc3fad7a509bd5a690ffb9f8dd
Andrii Karaivanskyi <karayvansky@gmail.com>
2018-06-05 12:33:09 -0700

    Added tests for Interview.FilterJson. Implementation TBD.

--- Interview.FilterJson/src/test/java/interview/FilterJsonTest.java
+++ Interview.FilterJson/src/test/java/interview/FilterJsonTest.java
@@ -1,8 +1,8 @@
+
 package interview;
 
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.List;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -16,17 +16,96 @@ import org.skyscreamer.jsonassert.JSONAssert;
 class FilterJsonTest {
 
     @Test
-    void empty() throws Exception {
-        List<DataExtractionAttribute> spec = Arrays.asList(new DataExtractionAttribute().name("test"));
-
-        checkJsonFiltering(spec, "empty", "empty");
+    void empty1() throws Exception {
+        checkJsonFiltering("empty", "empty", dea("test"));
     }
 
-    private void checkJsonFiltering(List<DataExtractionAttribute> spec, String inpFile, String expFile) throws JSONException {
+    @Test
+    void empty2() throws Exception {
+        checkJsonFiltering("all-cases", "empty", dea("miss"));
+    }
+
+    @Test
+    void empty3() throws Exception {
+        checkJsonFiltering("all-cases", "empty" /* no DEA, filter all */);
+    }
+
+    @Test
+    void nullObject() throws Exception {
+        DataExtractionAttribute dea = dea("null-node");
+        checkJsonFiltering("all-cases", "null-node", dea);
+
+        // ignore, since null object cannot have children
+        dea.addChildrenItem(dea("child"));
+        checkJsonFiltering("all-cases", "empty", dea);
+    }
+
+    @Test
+    void fullObjectIfNoChildrenInSpec() throws Exception {
+        DataExtractionAttribute dea = dea("not-null-node");
+        checkJsonFiltering("all-cases", "not-null-node", dea);
+
+        // we ignore node if no children found
+        dea.addChildrenItem(new DataExtractionAttribute().name("missing-child"));
+        checkJsonFiltering("all-cases", "empty", dea);
+    }
+
+    @Test
+    void filterByChildren() throws Exception {
+        DataExtractionAttribute dea = dea("not-null-node") //
+                .addChildrenItem(dea("not-null-node-ch1")) //
+                .addChildrenItem(dea("not-null-node-ch2")) //
+                .addChildrenItem(dea("not-null-node-ch3"));
+
+        // filter out "not-null-node-ch4"
+        checkJsonFiltering("all-cases", "not-null-node.no-ch4", dea);
+    }
+
+    @Test
+    void filterByChildrenInDepth() throws Exception {
+        DataExtractionAttribute dea = dea("not-null-node").addChildrenItem(dea("not-null-node-ch3") //
+                .addChildrenItem(dea("not-null-node-ch3-ch1")) //
+                .addChildrenItem(dea("not-null-node-ch3-other")));
+
+        checkJsonFiltering("all-cases", "not-null-node-ch3", dea);
+
+        // remove "not-null-node-ch3-ch1", and retain "not-null-node-ch3-other"
+        // since there is no such node as "not-null-node-ch3-other", filter all
+        dea.getChildren().get(0).getChildren().remove(0);
+        checkJsonFiltering("all-cases", "empty", dea);
+    }
+
+    @Test
+    void multipleRootAttributes() throws Exception {
+        checkJsonFiltering("all-cases", "null-node.and.not-null-node", dea("not-null-node"), dea("null-node"));
+    }
+
+    @Test
+    void arrayInChild() throws Exception {
+        DataExtractionAttribute dea = dea("arr1");
+        checkJsonFiltering("all-cases", "arr1", dea);
+
+        dea.addChildrenItem(dea("arr1-ch3"));
+        checkJsonFiltering("all-cases", "arr1-ch3", dea);
+
+        dea.getChildren().remove(0);
+        dea.addChildrenItem(dea("missing-child"));
+        checkJsonFiltering("all-cases", "empty", dea);
+    }
+
+    @Test
+    void arrayInRoot() throws Exception {
+        checkJsonFiltering("all-cases", "arr2-ch1", dea("arr2-ch1"));
+
+        checkJsonFiltering("all-cases", "arr2", dea("arr2-ch1"), dea("arr2-ch2"));
+    }
+
+    private void checkJsonFiltering(String inpFile, String expFile, DataExtractionAttribute... specs)
+            throws JSONException {
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-        
-        FilterJson.extract(jsonArrayFromFile(inpFile), spec , arrayBuilder);
-        
+
+        FilterJson.extract(jsonArrayFromFile(inpFile), Arrays.asList(specs), arrayBuilder);
+
         JSONAssert.assertEquals(jsonArrayFromFile(expFile).toString(), arrayBuilder.build().toString(), true);
     }
 
@@ -35,4 +114,9 @@ class FilterJsonTest {
         JsonReader jsonReader = Json.createReader(is);
         return jsonReader.readArray();
     }
+
+    protected DataExtractionAttribute dea(String name) {
+        return new DataExtractionAttribute().name(name);
+    }
+
 }
